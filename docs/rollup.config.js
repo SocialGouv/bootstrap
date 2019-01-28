9d10cf4468d9d2483ffd46c382e17068b38c7a9f
Douglas Duteil <douglasduteil@gmail.com>
2019-01-28 23:31:22 +0100

    improv(docs): add autoprefixer and rollup js

---
+++ docs/rollup.config.js
@@ -0,0 +1,20 @@
+//
+
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+
+//
+
+export default {
+  input: "_js/main.js",
+  output: {
+    file: "assets/js/main.js",
+    format: "iife"
+  },
+  plugins: [
+    resolve({}),
+    babel({
+      exclude: "node_modules/**"
+    })
+  ]
+};
