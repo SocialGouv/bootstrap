c53e3482ec0787daebabbf6ad7dfe5d83c74127e
Douglas Duteil <douglasduteil@gmail.com>
2019-01-29 04:46:12 +0100

    test(core): add non-regression tests

---
+++ packages/core/__test__/regressions/index.spec.js
@@ -0,0 +1,52 @@
+// @ts-check
+
+const { promisify } = require("util");
+const { join } = require("path");
+const { readFileSync } = require("fs");
+const sass = require("node-sass");
+
+//
+
+const cwd = join(__dirname, "../..");
+
+const includePaths = [
+  join(cwd, "src"),
+  join(cwd, "node_modules"),
+  join(cwd, "../../node_modules")
+];
+const sassOptions = {
+  importer: require("node-sass-yaml-importer"),
+  includePaths
+};
+
+const defaultFiles = [
+  "bootstrap/scss/functions",
+  "colors",
+  "variables",
+  "bootstrap/scss/mixins"
+];
+const importContext = defaultFiles.reduce(
+  (memo, file) => `${memo}\n@import "${file}";`,
+  ""
+);
+const mainFile = join(cwd, "src/design-beta-gouv.scss");
+const importedFiles = readFileSync(mainFile, { encoding: "utf8" })
+  .match(/@import\s+"([^"]+)";/g)
+  .map(importLine => {
+    const [, file] = importLine.match(/@import\s+"([^"]+)";/);
+    return file;
+  })
+  .filter(file => !defaultFiles.includes(file));
+
+//
+
+test.each(importedFiles)("%s should match snapshot", async name => {
+  const result = await promisify(sass.render)({
+    ...sassOptions,
+    data: `
+      ${importContext}
+      @import "${name}";
+    `
+  });
+  expect(result.css.toString()).toMatchSnapshot(name);
+});
